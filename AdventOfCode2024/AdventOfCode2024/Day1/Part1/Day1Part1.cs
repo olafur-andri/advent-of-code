e1bbd21f8d3a92a5a4b94f37c512e2cfccdd9722
Ólafur Andri Davíðsson <olafur1199@gmail.com>
2024-12-23 17:41:37 +0000

    First commit in 2024. Finished day 1, part1

---
+++ AdventOfCode2024/AdventOfCode2024/Day1/Part1/Day1Part1.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventOfCode2024.Day1.Part1;
+
+public static class Day1Part1
+{
+    public static void Run()
+    {
+        var input = Input.GetFromInputFile();
+        
+        List<int> sortedLeftList = input.LeftList.OrderBy(x => x).ToList();
+        List<int> sortedRightList = input.RightList.OrderBy(x => x).ToList();
+        
+        int result = sortedLeftList
+            .Zip(sortedRightList, (left, right) => Math.Abs(left - right)).Sum();
+        
+        Console.WriteLine(result);
+    }
+}
---
+++ AdventOfCode2024/AdventOfCode2024/Day1/Part1/Input.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024.Day1.Part1;
+
+public partial record Input(IReadOnlyList<int> LeftList, IReadOnlyList<int> RightList)
+{
+    [GeneratedRegex(@"\s+")]
+    private static partial Regex OneOrMoreWhitespacesRegex();
+    
+    public static Input GetFromInputFile()
+    {
+        var leftList = new List<int>();
+        var rightList = new List<int>();
+
+        foreach (string line in File.ReadAllLines(@"Day1\Part1\input.txt"))
+        {
+            string[] splitLine = OneOrMoreWhitespacesRegex().Split(line);
+            var (leftNumber, rightNumber) = (Convert.ToInt32(splitLine[0]), Convert.ToInt32(splitLine[1]));
+            
+            leftList.Add(leftNumber);
+            rightList.Add(rightNumber);
+        }
+        
+        return new Input(leftList, rightList);
+    }
+}
